9ad6df0ab8070bda04932365c28baa4e5f1d722a
greta-dasmeta-com <greta@dasmeta.com>
2024-11-26 16:30:05 +0400

    feat(DMVP-6117): Remove dividing from the queries as we use unit

--- modules/dashboard/modules/widgets/pod/memory/base.tf
+++ modules/dashboard/modules/widgets/pod/memory/base.tf
@@ -19,9 +19,9 @@ module "base" {
   }
 
   metrics = [
-    { label = "Avg", color = "FFC300", expression = "avg(rate(container_memory_usage_bytes{pod=~\"${var.pod}-[^-]+-[^-]+$\", namespace=\"${var.namespace}\"}[${var.period}m])) / 1024 / 1024 * 100" },
-    { label = "Max", color = "FF774D", expression = "max(rate(container_memory_usage_bytes{pod=~\"${var.pod}-[^-]+-[^-]+$\", namespace=\"${var.namespace}\"}[${var.period}m])) / 1024 / 1024 * 100"},
-    { label = "Request", color = "007CEF", expression = "max(kube_pod_container_resource_requests{pod=~\"${var.pod}-[^-]+-[^-]+$\", namespace=\"${var.namespace}\", resource=\"memory\"}) / 1024 / 1024" },
-    { label = "Limit", color = "FF0F3C", expression = "max(kube_pod_container_resource_limits{pod=~\"${var.pod}-[^-]+-[^-]+$\", namespace=\"${var.namespace}\", resource=\"memory\"}) / 1024 / 1024" },
+    { label = "Avg", color = "FFC300", expression = "avg(rate(container_memory_usage_bytes{pod=~\"${var.pod}-[^-]+-[^-]+$\", namespace=\"${var.namespace}\"}[${var.period}m]))" },
+    { label = "Max", color = "FF774D", expression = "max(rate(container_memory_usage_bytes{pod=~\"${var.pod}-[^-]+-[^-]+$\", namespace=\"${var.namespace}\"}[${var.period}m]))"},
+    { label = "Request", color = "007CEF", expression = "max(kube_pod_container_resource_requests{pod=~\"${var.pod}-[^-]+-[^-]+$\", namespace=\"${var.namespace}\", resource=\"memory\"})" },
+    { label = "Limit", color = "FF0F3C", expression = "max(kube_pod_container_resource_limits{pod=~\"${var.pod}-[^-]+-[^-]+$\", namespace=\"${var.namespace}\", resource=\"memory\"})" },
   ]
 }
